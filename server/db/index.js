8a5d8bf2c27f57632d4ef5c21abc350ec30b1a02
Daniel Reed <daniel.reed998@gmail.com>
2019-09-25 10:14:15 -0500

    add localhost db address

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/apax_weather', { useNewUrlParser: true });
 
 let db = mongoose.connection;
 
